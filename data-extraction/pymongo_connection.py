d41e89996c9a60864d508207ad4cb595a9f436e1
roberthan <roberthan818@gmail>
2013-01-26 20:15:33 -0800

    pymongo connection

---
+++ data-extraction/pymongo_connection.py
@@ -0,0 +1,6 @@
+from pymongo import Connection
+
+connection = Connection('54.235.168.23', 27017)
+db = connection.opendatagov
+#db.auth("mongoadmin", "anadminpassword")
+raw_catalog = db.raw_catalog
